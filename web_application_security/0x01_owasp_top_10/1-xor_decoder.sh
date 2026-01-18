4793f38ea1107c83cc58e4d6d73164de8a464967
Amir Asadli <asadliamir800@gmail.com>
2026-01-18 14:46:48 +0400

    Changes Saved

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo -n "${1#\{xor\}}" | base64 --decode | perl -pe '$_=pack("C*", map {$_^0xA5} unpack("C*", $_))'
+python3 -c "from base64 import b64decode; print(bytes(byte ^ 0x5f for byte in b64decode('$1'.replace('{xor}', ''))).decode('utf-8'))"
