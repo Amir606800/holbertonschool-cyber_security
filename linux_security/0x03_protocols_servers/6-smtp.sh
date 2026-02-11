b67c71330e2ed5d6b0b297160ba28e05a5245c33
Amir Asadli <asadliamir800@gmail.com>
2026-02-11 16:33:25 +0400

    Changes Saved

--- linux_security/0x03_protocols_servers/6-smtp.sh
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -qE "smtpd_tls_security_level\s*=\s*(may|encrypt)" /etc/postfix/main.cf && grep "smtpd_tls_security_level" /etc/postfix/main.cf || echo "STARTTLS not configured"
+grep -qE "smtpd_tls_security_level\s*=\s*(may|encrypt)" /etc/postfix/main.cf && grep "^smtpd_tls_security_level" /etc/postfix/main.cf || echo "STARTTLS not configured"
