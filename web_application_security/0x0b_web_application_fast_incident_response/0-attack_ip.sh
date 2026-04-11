340d9e21437bfc3c25f4bc2c0bc28281d6edc6a1
Amir Asadli <asadliamir800@gmail.com>
2026-04-11 17:20:35 +0400

    Changes Saved

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -oE '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' "logs.txt" | sort | uniq -c | sort -rn
+grep -oE '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' $1 | sort | uniq -c | sort -rn | head -1 | awk '{print $2}'
