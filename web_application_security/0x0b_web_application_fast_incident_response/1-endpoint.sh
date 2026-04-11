a139fc9771a29515109c501a9a08b8b0d431006a
Amir Asadli <asadliamir800@gmail.com>
2026-04-11 17:27:56 +0400

    Changes Saved

--- web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/1-endpoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -oE '"(GET|POST|PUT|DELETE|PATCH) [^ ]+' $1 | awk '{print $2}' | sort | uniq -c | sort -rn
+grep -oE '"(GET|POST|PUT|DELETE|PATCH) [^ ]+' $1 | awk '{print $2}' | sort | uniq -c | sort -rn | head -1 | awk '{print $2}'
