d93d4a4cc82c8ea4413c80a742058cfdc2ee2492
Amir Asadli <asadliamir800@gmail.com>
2026-05-24 09:08:48 +0400

    Changes Saved

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -8,7 +8,7 @@ def merge_json_files(file1_path, file2_path)
   file2_data = JSON.parse(File.read(file2_path))
 
   # Merge arrays (combine both datasets)
-  merged_data = file1_data + file2_data
+  merged_data = file2_data + file1_data
 
   # Write back into file2 (as required by your main script)
   File.write(file2_path, JSON.pretty_generate(merged_data))
