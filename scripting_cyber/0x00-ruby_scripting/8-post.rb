5f23449f268fae8b6aaa506ebce0072d7e7e56fc
Amir Asadli <asadliamir800@gmail.com>
2026-05-24 09:30:56 +0400

    Changes Saved

---
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require 'net/http'
+require 'json'
+require 'uri'
+
+def post_request(url, body_params)
+  uri = URI.parse(url)
+
+  # Create HTTP object
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = (uri.scheme == "https")
+
+  # Create POST request
+  request = Net::HTTP::Post.new(uri.path, { 'Content-Type' => 'application/json' })
+
+  # Attach JSON body
+  request.body = body_params.to_json
+
+  # Send request
+  response = http.request(request)
+
+  # Print response status
+  puts "Response status: #{response.code} #{response.message}"
+
+  # Print response body as formatted JSON
+  puts "Response body:"
+  puts JSON.pretty_generate(JSON.parse(response.body))
+end
