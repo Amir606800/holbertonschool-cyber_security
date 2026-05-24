e089d8e5481dc566d84c3eacd54e2707579a2ce4
Amir Asadli <asadliamir800@gmail.com>
2026-05-24 08:58:00 +0400

    Changes Saved

---
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require "json"
+
+def count_user_ids(path)
+    data = JSON.parse(File.read(path))
+    count = 0
+
+    data.each do |item|
+        count += 1 if item.key?("userId")
+    end
+
+    count
+end
