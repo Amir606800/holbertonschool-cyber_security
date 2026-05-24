940219e71be8fed08140f3c35159b3c4ad3c37fd
Amir Asadli <asadliamir800@gmail.com>
2026-05-24 08:59:20 +0400

    Changes Saved

--- scripting_cyber/0x00-ruby_scripting/3-read_file.rb
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -1,14 +1,15 @@
 #!/usr/bin/env ruby
 
-require "json"
+require 'json'
 
 def count_user_ids(path)
-    data = JSON.parse(File.read(path))
-    count = 0
+  data = JSON.parse(File.read(path))
 
-    data.each do |item|
-        count += 1 if item.key?("userId")
-    end
+  count = 0
 
-    count
+  data.each do |item|
+    count += 1 if item.key?("userId")
+  end
+
+  count
 end
